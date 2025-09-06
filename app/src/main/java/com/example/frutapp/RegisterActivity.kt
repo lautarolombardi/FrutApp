60628fd40c8006263dc04d5e29635003eb8a64b8
Lola Torres Rodriguez <lolatorresrodr@gmail.com>
2025-09-06 16:06:43 -0300

    Implementación login y registro

---
+++ app/src/main/java/com/example/frutapp/RegisterActivity.kt
@@ -0,0 +1,28 @@
+package com.example.frutapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val tvReturnLogin = findViewById<TextView>(R.id.tvHaveAnAccount)
+        tvReturnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}

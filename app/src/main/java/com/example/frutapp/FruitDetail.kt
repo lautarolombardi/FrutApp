fa7b93bbf36a66432bd12d512ac3a2724c9c6e3d
Marco Pappalardo <marco.pappalardou2003qgmail.com>
2025-09-07 16:02:54 -0300

    Feat : fruit detail view

--- app/src/main/java/com/example/frutapp/FruitDetail.kt
+++ app/src/main/java/com/example/frutapp/FruitDetail.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
 
 class FruitDetail : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,52 @@ class FruitDetail : AppCompatActivity() {
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
       insets
     }
+    val bundle: Bundle? = intent.extras
+    if (bundle != null) {
+      val intentNameValue = bundle?.getString("name")
+      val intentEmoji = bundle?.getString("emoji")
+
+      val intentFamilyValue = bundle?.getString("family")
+      val intentGenusValue = bundle?.getString("genus")
+      val intentOrderValue = bundle?.getString("order")
+
+      val intentCarbohydratesValue = bundle?.getDouble("carbohydrates")
+      val intentProteinValue = bundle?.getDouble("protein")
+      val intentFatValue = bundle?.getDouble("fat")
+      val intentCaloriesValue = bundle?.getInt("calories")
+      val intentSugarValue = bundle?.getDouble("sugar")
+
+
+      val emoji: TextView = findViewById(R.id.tvEmoji)
+      emoji.text = intentEmoji.toString()
+
+      val name: TextView = findViewById(R.id.tvNameValue)
+      name.text = intentNameValue.toString()
+
+      val family: TextView = findViewById(R.id.tvFamilyValue)
+      family.text = intentFamilyValue.toString()
+
+      val genus: TextView = findViewById(R.id.tvGenusValue)
+      genus.text = intentGenusValue.toString()
+
+      val order: TextView = findViewById(R.id.tvOrdenValue)
+      order.text = intentOrderValue.toString()
+
+      val carbohydrates: TextView = findViewById(R.id.tvCarbohydratesValue)
+      carbohydrates.text = intentCarbohydratesValue.toString()
+
+      val protein: TextView = findViewById(R.id.tvProteinValue)
+      protein.text = intentProteinValue.toString()
+
+      val fat: TextView = findViewById(R.id.tvFatValue)
+      fat.text = intentFatValue.toString()
+
+      val calories: TextView = findViewById(R.id.tvCaloriesValue)
+      calories.text = intentCaloriesValue.toString()
+
+      val sugar: TextView = findViewById(R.id.tvSugarValue)
+      sugar.text = intentSugarValue.toString()
+    }
+
   }
 }
